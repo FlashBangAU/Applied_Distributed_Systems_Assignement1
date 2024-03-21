ed1c7e75521eb52a294a12a39e599da991101389
Hughen Flint <104950296+FlashBangAU@users.noreply.github.com>
2024-03-22 08:06:48 +1000

    UDPServer Filled With Course Material to manipulate

--- UDPServer/src/udpserver/UDPServer.java
+++ UDPServer/src/udpserver/UDPServer.java
@@ -4,17 +4,46 @@
  */
 package udpserver;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 /**
  *
  * @author User
  */
 public class UDPServer {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String args[]) {
+        DatagramSocket aSocket = null;
+        try {
+            aSocket = new DatagramSocket(6789);
+            byte[] buffer = new byte[1000];
+            while (true) {
+                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+                aSocket.receive(request);
+                //System.out.println("Client Request: " + new String(request.getData(), 0, request.getLength()));
+
+                System.out.println("Client Request: "
+                        + new String(request.getData()) + " server at port "
+                        + request.getPort() + " at IP " + request.getAddress());
+                    
+                    DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(),
+                        request.getAddress(), request.getPort());
+
+                aSocket.send(reply);
+            }
+        } catch (SocketException e) {
+            System.out.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO: " + e.getMessage());
+        }
     }
-    
+
+//        finally {
+//           if (aSocket != null) {
+//            aSocket.close();
+//        }
+//    }
 }
