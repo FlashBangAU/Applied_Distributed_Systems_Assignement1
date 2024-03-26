195aa2ed96b20ea510d8dc7c28c09fd674201ca2
Hughen Flint <104950296+FlashBangAU@users.noreply.github.com>
2024-03-26 21:01:23 +1000

    Member class added serialisable code that is ran every 2 seconds.

--- Assignment01/src/tcpserver/Member.java
+++ Assignment01/src/tcpserver/Member.java
@@ -4,11 +4,23 @@
  */
 package tcpserver;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import static java.lang.System.out;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author User
  */
-public class Member {
+public class Member implements Serializable {
+
     String firstName;
     String lastName;
     String address;
@@ -53,10 +65,48 @@ public class Member {
         this.phNumber = phNumber;
     }
 
-    @Override
-    public String toString() {
-        return (firstName +":"+ lastName +":" + address + ":" + phNumber);
+    public static String memberSerialization() throws IOException {
+        int i = 0;
+        ObjectOutputStream out = null;
+
+        String filename = "memberlistObject";
+        
+        FileOutputStream fos = null;
+
+        //out.reset();
+
+        try {
+            File myObj = new File("memberList.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                if (i==0){
+                fos = new FileOutputStream(filename, false);
+                }else{
+                    fos = new FileOutputStream(filename, true);
+                }
+                i++;
+                out = new ObjectOutputStream(fos);
+
+                String data = myReader.nextLine();
+                System.out.println(data);
+
+                String[] memberArray = data.split(":");
+
+                Member member1 = new Member(memberArray[0], memberArray[1], memberArray[2], memberArray[3]);
+
+                try {
+                    out.writeObject(member1);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+            out.close();
+            System.out.println("Object Persisted");
+            System.out.println();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(UpdateMemberlistObject.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return "";
     }
-    
-    
 }
