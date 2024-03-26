162c11bb31ed87ade9e0194554b4263ae2139894
Hughen Flint <104950296+FlashBangAU@users.noreply.github.com>
2024-03-26 21:02:03 +1000

    Added 2 second timer for serialization.

--- Assignment01/src/tcpserver/TCPServer.java
+++ Assignment01/src/tcpserver/TCPServer.java
@@ -10,42 +10,73 @@ package tcpserver;
  * and open the template in the editor.
  */
 //package TCPConnection;
-
 /**
  *
  * @author Mary
  */
-
 import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TCPServer {
 
     public static void main(String args[]) {
+        int interval = 2000; //Print to file every 2 seconds
+
+        java.util.Timer timer = new java.util.Timer(); // using timer from util package
+
+        //schedule timer to write to file after interval and repeat every interval
+        timer.schedule(new UpdateMemberlistObject(), interval, interval);
+
         try {
-            int serverPort = 7896;
+            int serverPort = 1130;
             ServerSocket listenSocket = new ServerSocket(serverPort);
             while (true) {
                 //listening for client
                 Socket clientSocket = listenSocket.accept();
                 Connection c = new Connection(clientSocket);
-                System.out.printf("\nServer waiting on: %d for client from %d ",
+                System.out.printf("\nServer waiting on: %d for client from %d " + "\n",
                         listenSocket.getLocalPort(), clientSocket.getPort());
             }
         } catch (IOException e) {
             System.out.println("Listen :" + e.getMessage());
         }
     }
-}
-//This class is to be used by TcpServer class, not public.
 
+}
+
+class UpdateMemberlistObject extends TimerTask {
+
+    //this method is called automatically when the task is scheduled
+    public void run() {
+        try {
+            String member = Member.memberSerialization();
+        } catch (IOException ex) {
+            Logger.getLogger(UpdateMemberlistObject.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+
+//This class is to be used by TcpServer class, not public.
 class Connection extends Thread {
 
     DataInputStream in;
@@ -69,12 +100,10 @@ class Connection extends Thread {
         try { // an echo server
             //data recieved
             String data = in.readUTF();
+
             //data saved to file
-            Writer output;
-            output = new BufferedWriter(new FileWriter("memberList.txt", true));
-            output.append(data + "\n");
-            output.close();
-            
+            writeToFile(data);
+
             //data is sent back to client
             out.writeUTF("Save Data of the member number: ");
 
@@ -88,5 +117,12 @@ class Connection extends Thread {
             } catch (IOException e) {/*close failed*/
             }
         }
+    }
+
+    public void writeToFile(String data) throws IOException {
+        Writer output;
+        output = new BufferedWriter(new FileWriter("memberList.txt", true));
+        output.append(data + "\n");
+        output.close();
     }
 }
