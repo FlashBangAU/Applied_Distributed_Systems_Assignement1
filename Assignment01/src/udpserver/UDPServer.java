6bb14be14841da98b11794282cd17bb9ba1845e2
Hughen Flint <104950296+FlashBangAU@users.noreply.github.com>
2024-03-30 01:43:26 +1000

    Majority of UDPServer Code unable to collect multipule objects as this time

--- Assignment01/src/udpserver/UDPServer.java
+++ Assignment01/src/udpserver/UDPServer.java
@@ -4,10 +4,16 @@
  */
 package udpserver;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import tcpserver.Member;
 
 /**
  *
@@ -15,21 +21,30 @@ import java.net.SocketException;
  */
 public class UDPServer {
 
-    public static void main(String args[]) {
+    public static void main(String args[]) throws ClassNotFoundException {
         DatagramSocket aSocket = null;
+        byte[] data = null;
+        final byte[] buffer;
+
+        //retrieve client message and return member data to client
         try {
-            aSocket = new DatagramSocket(6789);
-            byte[] buffer = new byte[1000];
+            aSocket = new DatagramSocket(2230);
+            buffer = new byte[1000];
             while (true) {
                 DatagramPacket request = new DatagramPacket(buffer, buffer.length);
                 aSocket.receive(request);
-                //System.out.println("Client Request: " + new String(request.getData(), 0, request.getLength()));
 
+                //Server recieves message from client
                 System.out.println("Client Request: "
-                        + new String(request.getData()) + " server at port "
-                        + request.getPort() + " at IP " + request.getAddress());
-                    
-                    DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(),
+                        + new String(request.getData()));
+
+                //if true will enter and prepare response to client
+                if (Arrays.equals(request.getData(), request.getData())) {
+                    data = memberDeserialization();
+                }
+
+                //reply is sent back
+                DatagramPacket reply = new DatagramPacket(data, data.length,
                         request.getAddress(), request.getPort());
 
                 aSocket.send(reply);
@@ -38,12 +53,79 @@ public class UDPServer {
             System.out.println("Socket: " + e.getMessage());
         } catch (IOException e) {
             System.out.println("IO: " + e.getMessage());
+        } finally {
+            if (aSocket != null) {
+                aSocket.close();
+            }
         }
+
     }
 
-//        finally {
-//           if (aSocket != null) {
-//            aSocket.close();
-//        }
-//    }
+    //collects memberObjects in arrayList to send as byte to client
+    public static byte[] memberDeserialization() throws IOException, ClassNotFoundException {
+        ArrayList<String> details = new ArrayList<>();
+
+        final String fileName = "memberlistObject";
+        Member member = null;
+        FileInputStream fis = new FileInputStream(fileName);
+        ObjectInputStream in = new ObjectInputStream(fis);
+        boolean cont = true;
+
+        //loops through to add member Object in file to arraylist
+        try {
+            member = (Member) in.readObject();
+            while (cont) {
+
+                if (member != null) {
+
+                    System.out.println(member.getFirstName());
+                    System.out.println();
+
+                    details.add(member.getFirstName());
+                    details.add(member.getLastName());
+                    details.add(member.getAddress());
+                    details.add(member.getPhNumber());
+                } else {
+                    cont = false;
+                }
+
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        //prints last member processed
+        System.out.println("Person First Name: " + member.getFirstName());
+        System.out.println("Person Last Name: " + member.getLastName());
+        System.out.println("Person Address: " + member.getAddress());
+        System.out.println("Person Phone Number: " + member.getPhNumber());
+
+//        //converts members to byte for UDP 
+//        StringBuilder str = new StringBuilder();
+//        str.append(details);
+//        System.out.println(str);
+//        byte[] table = str.toString().getBytes();
+        StringBuilder msg = new StringBuilder();
+        System.out.println(msg);
+        for (String eachstring : details) {
+
+            // Each element in ArrayList followed by a colon
+            // followed by colon
+            msg.append(eachstring).append(":");
+        }
+
+        //StringBuilder becomes String
+        String message = msg.toString();
+
+        //last colon is removed
+        if (message.length() > 0) {
+            message = message.substring(0, message.length() - 1);
+        }
+        System.out.println(message);
+
+        //message is converted as byte and returned
+        byte[] table = message.getBytes();
+        in.close();
+        return table;
+    }
 }
